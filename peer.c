55e375261fa46bd586130d7f84179a3567198fc3
Mustafa <cs.mustafaali@gmail.com>
2024-10-08 04:51:03 -0700

    made changes to program 2

--- peer.c
+++ peer.c
@@ -69,12 +69,10 @@ FileList fileCounter(void) {
   // Print the hex representation of file names
   printf("Hex representation of files:\n");
   for (int i = 0; i < count; i++) {
-    printf("File %d (%s): ", i + 1, charArr[i]);
-    for (int j = 0; j < strlen(charArr[i]); j++) {
-      printf("0x%02x ", (unsigned char)charArr[i][j]);
+      printf("0x%02x ", (unsigned char)charArr[i]);
     }
     printf("\n");
-  }
+
 
   // Close the directory stream
   closedir(dir);
